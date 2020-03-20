8a4e770a2c37debfd456af9bd8a94d8947973863
Emmanuel Arias <eamanu@yaerobi.com>
2020-03-20 20:05:46 -0300

    finished product and utils

--- pyamazonlandsat/product.py
+++ pyamazonlandsat/product.py
@@ -1,14 +1,31 @@
 import attrs
+import os
+import tarfile
+
+from pyamazonlandsat.utils import get_path_row_from_name
+from pyamazonlandsat.downloader import Downloader
 
 
 class Product:
-    nombre = attrs.ib()
+    name = attrs.ib()
+    output_path = attrs.ib()
     _path_files = attrs.ib(init=False)
+    _link = attrs.ib(init=False, default='')
+
+    def _generate_link(self):
+        path, row = get_path_row_from_name(self.name)
+        self._link = 'c1/L8/%s/%s/%s' % (path, row, self.name)
 
     def _compress_product(self):
-        pass
+        with tarfile.open(
+            os.path.join(self.output_path, self.name), 'w') as tar:
+            for ff in os.listdir(self._path_files):
+                tar.add(
+                    os.path.join(
+                    self._path_files, ff),
+                    ff)
 
-    def get_image_product(self, name):
-        pass
-
-
+    def get_image_product(self):
+        downloader = Downloader(self._link)
+        self._path_files = downloader.download_images()
+        self._compress_product()
--- pyamazonlandsat/utils.py
+++ pyamazonlandsat/utils.py
@@ -5,6 +5,19 @@ def get_path_row_from_name(name):
     lname = to_list_name(name)
     path = lname[2][:3]
     row = lname[2][3:]
+    return (path, row)
+
+def get_acquisition_time(name):
+    return to_list_name(name)[3]
+
+def get_processing_date(name):
+    return to_list_name(name)[4]
+
+def get_collection_number(name):
+    return to_list_name(name)[5]
+
+def get_collection_category(name):
+    return to_list_name(name)[6]
 
 def get_satellite(name):
     return to_list_name(name)[0]
