bf4a38ec2a0279611b5eb8fd27f16f6de1d64f4f
Emmanuel Arias <eamanu@yaerobi.com>
2020-03-20 00:22:06 -0300

    finished downloader

--- pyamazonlandsat/downloader.py
+++ pyamazonlandsat/downloader.py
@@ -3,6 +3,7 @@ import re
 
 from bs4 import BeautifulSoup
 from urllib import request
+from tempfile import mkdtemp
 
 class Downloader:
     link = attrs.ib()
@@ -10,25 +11,45 @@ class Downloader:
     _soup = attrs.ib(init=False, default=None)
     _links_tiff = attrs.ib(init=False, default=list())
     _links_mtl_ang_files = attrs.ib(init=False, default=list())
+    _tmp_folder = attrs.ib(init=False default=mkdtemp())
 
-    def read_key_value(self, pattern):
+    def __read_key_value(self, pattern):
         for link in self._soup.find_all('a'):
             value = link.get('href')
             if re.match(pattern, value):
                 yield value
 
     def read_link(self):
-        self._html = request.urlopen(self.link).read()
+        self._html = request.urlopen('%s/index.html' % self.link).read()
         self._soup = BeautifulSoup(self._html, 'html.parser')
 
     def get_download_links_tiff(self):
-        self._links_tiff.append(self.read_key_value('.*.TIF'))
+        self._links_tiff.append(self.__read_key_value('.*.TIF'))
 
     def get_mtl_ang_files(self):
-        self._links_mtl_ang_files.append(self.read_key_value('.*.txt'))
+        self._links_mtl_ang_files.append(self.__read_key_value('.*.txt'))
 
-    def download_image(self):
-        pass
+    def _prepare_for_download(self):
+        self.read_link()
+        self.get_download_links_tiff()
+        self.get_mtl_ang_files()
+        return True
 
+    def _save_file_on_tmp_folder(self, name, content):
+        with open(os.path.join(self._tmp_folder, name), 'wb') as f:
+            f.write(content)
 
+    def _download_file(self, name):
+        file_content = requests.get('%s/%s' % (self.link, name),
+                                    allow_redirect=True)
+        self._save_file_on_tmp_folder(name, file_content)
 
+    def download_images(self):
+        self._prepare_for_download()
+
+        for tiff in self._links_tiff:
+            self._download_file(tiff)
+        for txt in self._links_mtl_ang_files:
+            self._download_file(txt)
+
+        return self._tmp_folder
